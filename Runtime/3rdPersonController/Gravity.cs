ea79b73c573f11d0bfc72a5ddbc6245b17b0a524
SelimDenizZayene <denizselimzayene@gmail.com>
2023-12-27 16:43:58 +0300

    added fall and jump animations and implemented isgrounded animation code

--- Runtime/3rdPersonController/Gravity.cs
+++ Runtime/3rdPersonController/Gravity.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,24 @@ using UnityEngine;
 public class Gravity : MonoBehaviour
 {
 	[SerializeField]
-	private float mass = 2f;
-
+	private float gravity = -20.0f;
 	[SerializeField]
-	private float gravity = -9.81f;
+	private float falldelay = 0.25f;
+	private float fallTimer = 0.0f;
+	[SerializeField]
+	private float groundOffset = 0.01f;
 
 	private Vector3 fallVelocity = Vector3.zero;
 	private CharacterController characterController = null;
+	
+	private bool wasFalling = false;
+	public event Action OnLanding;
+	public event Action OnFallStart;
+	
+	//Animation Settings
+	private Animator animator = null;
+	private bool isGrounded = true;
+	private bool jump = false;
 
 	private void Awake()
 	{
@@ -19,29 +31,90 @@ public class Gravity : MonoBehaviour
 		{
 			characterController = gameObject.GetComponent<CharacterController>();
 		}
+		
+		if (animator == null)
+		{
+			animator = gameObject.GetComponent<Animator>();
+		}
+	}
+	
+	private void OnEnable() {
+		OnFallStart += () => { Debug.Log("Falling"); };
+		OnLanding += () => { Debug.Log("Landing"); };
 	}
 
-	private void Update(){
-	
-		//reset fall velocity if grounded
-		if (characterController.isGrounded)
+	private void FixedUpdate()
+	{
+		Animate();
+		
+		// Reset fall velocity if grounded
+		// TODO: Use raycast instead of characterController.isGrounded
+		if (IsGrounded())
 		{
+			if (wasFalling)
+			{
+				OnLanding?.Invoke();
+				wasFalling = false;
+			}
+
+			fallTimer = 0.0f;
 			fallVelocity = Vector3.zero;
 			return;
 		}
 
 		Fall();
 	}
+
+	private bool IsGrounded()
+	{
+		// Perform a sphere check to check if the character is grounded
+		Vector3 sphereCenter = new Vector3(transform.position.x, transform.position.y - groundOffset, transform.position.z);
+		
+		if (Physics.CheckSphere(sphereCenter, groundOffset, LayerMask.GetMask("Ground")))
+		{
+			isGrounded = true;
+		}
+
+		else
+		{
+			isGrounded = false;
+		}
+		
+		return isGrounded;
+	}
 	
 	private void Fall()
 	{
-		//increase fall velocity if max velocity is not reached
-		if (fallVelocity.y > gravity * mass)
+		if(!wasFalling)
+			fallTimer += Time.deltaTime;
+			
+		if(fallTimer >= falldelay && !wasFalling)
 		{
-			fallVelocity += new Vector3(0f, gravity * mass * Time.deltaTime * 2f, 0f);
+			OnFallStart?.Invoke();
+			wasFalling = true;
+		}
+		//increase fall velocity if max velocity is not reached
+		if (fallVelocity.y > gravity)
+		{
+			fallVelocity += new Vector3(0f, gravity * Time.deltaTime * 5f, 0f);
+		}
+		else
+		{
+			fallVelocity = new Vector3(0f, gravity, 0f);
 		}
 		
 		//apply fall velocity
 		characterController.Move(fallVelocity * Time.deltaTime);
+	}
+	
+	private void Animate()
+	{
+		animator.SetBool("isGrounded", isGrounded);
+		animator.SetBool("jump", jump);
+	}
+	
+	private void OnDrawGizmos() {
+		Vector3 sphereCenter = new Vector3(transform.position.x, transform.position.y - groundOffset, transform.position.z);
+		Gizmos.DrawSphere(sphereCenter, groundOffset);
 	}
 }
--- Runtime/3rdPersonController/MovementController.cs
+++ Runtime/3rdPersonController/MovementController.cs
@@ -4,21 +4,26 @@ using UnityEngine.InputSystem;
 
 namespace Zayene.Character_Controller.Third_Person
 {
+	//fix: when quickly changing directions, movement becomes very fast until stopping
 	public class MovementController : MonoBehaviour
 	{
 		private ActionMaps actionMaps = null;
 		private CharacterController characterController = null;
 
 		[Header("Movement Settings")]
-		[Range(1f, 20f)]
-		public float movementSpeed = 10f;
-		public float backRunMulti = -1f;
-		public float backWalkMulti = -0.2f;
-		public float walkMulti = 0.2f;
-		public float runMulti = 1f;
-		public float sprintMulti = 2f;
+		[Range(5f, 15f)]
+		public float runSpeed = 10f;
+		
+		[Range(2f, 7f)]
+		public float walkSpeed = 5f;
+		// public float fallingMvtMulti = 0.3f;\
+		// public float backRunMulti = -1f;
+		// public float backWalkMulti = -0.2f;
+		// public float walkMulti = 0.2f;
+		// public float runMulti = 1f;
+		// public float sprintMulti = 2f;
 		public float currentMoveMulti = 0f;
-		public float turnSpeed = 10f;
+		public float turnSpeed = 15f;
 
 		private bool isMoving = false;
 
@@ -71,7 +76,6 @@ namespace Zayene.Character_Controller.Third_Person
 		
 		private void ReadMovement(InputAction.CallbackContext context)
 		{
-			Debug.Log("Move");
 			SetMoveMultiplier();
 			isMoving = true;
 			StartCoroutine(Move());
@@ -79,7 +83,6 @@ namespace Zayene.Character_Controller.Third_Person
 
 		private void StopReadMovement(InputAction.CallbackContext context)
 		{
-			Debug.Log("Stop");
 			isMoving = false;
 			currentMoveMulti = 0f;
 			StopCoroutine(Move());
@@ -89,31 +92,27 @@ namespace Zayene.Character_Controller.Third_Person
 
 		private void SetMoveMultiplier()
 		{
-			currentMoveMulti = runMulti;
+			currentMoveMulti = 1f;
 		}
 		
 		private IEnumerator Move()
 		{
 			while(isMoving)
 			{
-				Debug.Log("Moving");
-				 
 				Vector2 inputDirection = actionMaps.gameplay.Movement.ReadValue<Vector2>();
 				Vector3 cameraRotation = cameraPositioner.transform.forward;
 				cameraRotation.y = 0f; // ignore vertical rotation
 
-				//TODO: rotate player to movedirection and dont rotate camera
-
 				Vector3 moveDirection = cameraPositioner.transform.rotation * new Vector3(inputDirection.x, 0f, inputDirection.y);
 				moveDirection.y = 0f; // ignore vertical movement
 				
+				//rotate player to movedirection and dont rotate camera
 				Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
 				transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, turnSpeed * Time.deltaTime);
-				// cameraPositioner.transform.rotation = Quaternion.Lerp(cameraPositioner.transform.rotation, Quaternion.Inverse(toRotation), turnSpeed/5 * Time.deltaTime);
+			
+				//Todo: add different speeds
+				characterController.Move(moveDirection * runSpeed * Time.deltaTime);
 				
-				characterController.Move(moveDirection * movementSpeed * currentMoveMulti * Time.deltaTime);
-				// Debug.Log(moveDirection);
-
 				yield return new WaitForEndOfFrame();
 			}
 			yield return null;
